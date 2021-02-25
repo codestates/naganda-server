7080d23c9558e39f7139d296330042fdcf61dd9f
risha <risha939@gmail.com>
2021-02-25 14:13:57 +0900

    [feat] 검색기능 구현 && 라우터 연결

--- controllers/main/index.js
+++ controllers/main/index.js
@@ -1,3 +1,4 @@
 module.exports = {
     top4 : require('./top4'),
+    search : require('./search'),
 }
--- router/main.js
+++ router/main.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { top4 } = require('../controllers/main');
+const { top4, search } = require('../controllers/main');
 
 //* GET /main/top4
 router.get('/top4', top4.get);
+//* GET /main/search
+router.get('/search', search.get);
 
 module.exports = router;
