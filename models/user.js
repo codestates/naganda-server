df71307ca6b23e287672d063a352595bb0623a47
박수현 <tngus2057@naver.com>
2021-02-25 11:28:11 +0900

    [refactor] user모델의 validator 삭제

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+var users = new mongoose.Schema({
+	nickname: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		trim: true,
+		validate: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+	},
+	avatar: "string",
+});
+
+module.exports = mongoose.model("user", users);
