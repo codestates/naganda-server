da44b57be4548b7c579af2d9dc60672553788ba1
risha <risha939@gmail.com>
2021-02-26 16:53:03 +0900

    [feat] myScheduleInfo 엔드포인트 연결

--- controllers/mypage/index.js
+++ controllers/mypage/index.js
@@ -1,3 +1,4 @@
 module.exports = {
 	myInformation: require("./myInformation"),
+	myScheduleInfo: require("./myScheduleInfo")
 };
--- router/users.js
+++ router/users.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const { signup } = require("../controllers/main");
-const { myInformation } = require("../controllers/mypage");
+const { myInformation, myScheduleInfo } = require("../controllers/mypage");
 
 //* POST /users/signup
 router.post("/signup", signup.post);
 //* GET /users/:nickname
 router.get("/:nickname", myInformation.get);
+//* GET /users/schedules/:userid
+router.get("/schedules/:userid", myScheduleInfo.get);
 
 module.exports = router;
