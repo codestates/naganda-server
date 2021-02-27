26d6b02a3da4370702b2fa4bc798ed30d1d1bc46
박수현 <tngus2057@naver.com>
2021-02-27 11:21:21 +0900

    [refactor] myInformation API에 대한 경로파라미터 변경

--- router/users.js
+++ router/users.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const { signup } = require("../controllers/main");
-const { myInformation } = require("../controllers/mypage");
+const { myInformation, unregister } = require("../controllers/mypage");
 
 //* POST /users/signup
 router.post("/signup", signup.post);
-//* GET /users/:nickname
-router.get("/:nickname", myInformation.get);
+//* GET /users/:userid
+router.get("/:userid", myInformation.get);
+//* DELETE /users/delete/:userid
+router.delete("/delete/:userid", unregister.delete);
 
 module.exports = router;
