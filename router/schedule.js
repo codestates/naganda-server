26997c871524aef0c26af469c14294f1556cd07d
박수현 <tngus2057@naver.com>
2021-03-02 14:11:37 +0900

    [feat] JWT 미들웨어 작성 및 라우터에 적용

--- router/schedule.js
+++ router/schedule.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const { upload } = require('./multer');
+const auth = require("./auth");
+const { upload } = require("./multer");
 
-const { oneSchedule, bookmark, bestworst, modify, save } = require("../controllers/schedule");
+const {
+	oneSchedule,
+	bookmark,
+	bestworst,
+	modify,
+	save,
+} = require("../controllers/schedule");
 
 //* POST /schedule/bookmark/:scheduleid
-router.post("/bookmark/:scheduleid", bookmark.post);
+router.post("/bookmark/:scheduleid", auth, bookmark.post);
 //* GET /schedule/:scheduleid
 router.get("/:scheduleid", oneSchedule.get);
 //* PATCH /schedule/bestworst/:scheduleid
-router.post("/bestworst/:scheduleid", bestworst.post);
+router.post("/bestworst/:scheduleid", auth, bestworst.post);
 //* PUT /schedule/modify/:scheduleid
-router.put("/modify/:scheduleid", upload.array('img', 3), modify.put);
+router.put("/modify/:scheduleid", auth, upload.array("img", 3), modify.put);
 //* POST /schedule/save
-router.post("/save", upload.array('img', 3), save.post);
+router.post("/save", auth, upload.array("img", 3), save.post);
 
 module.exports = router;
--- router/users.js
+++ router/users.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { upload } = require('./multer');
+const auth = require("./auth");
+const { upload } = require("./multer");
 
+const { login, guestLogin } = require("../controllers/login");
 const { signup } = require("../controllers/main");
 const {
 	myInformation,
@@ -9,22 +11,31 @@ const {
 	removeSchedule,
 	unregister,
 	guestLogout,
-	updateUserInfo
+	updateUserInfo,
 } = require("../controllers/mypage");
 
+//* POST /users/login
+router.post("/login", login.post);
+//* POST /users/guest
+router.post("/guest", guestLogin.post);
 //* POST /users/signup
 router.post("/signup", signup.post);
 //* GET /users/:userid
-router.get("/:userid", myInformation.get);
+router.get("/:userid", auth, myInformation.get);
 //* GET /users/schedules/:userid
-router.get("/schedules/:userid", myScheduleInfo.get);
+router.get("/schedules/:userid", auth, myScheduleInfo.get);
 //* DELETE /users/removeSchedules/:scheduleid
-router.delete("/removeSchedules/:scheduleid", removeSchedule.delete);
+router.delete("/removeSchedules/:scheduleid", auth, removeSchedule.delete);
 //* DELETE /users/delete/:userid
-router.delete("/delete/:userid", unregister.delete);
+router.delete("/delete/:userid", auth, unregister.delete);
 //* POST /users/logout/:userid
-router.post("/logout/:userid", guestLogout.post);
+router.post("/logout/:userid", auth, guestLogout.post);
 //* PATCH /users/updateUserinfo/:userid
-router.patch("/updateUserinfo/:userid", upload.single('img'), updateUserInfo.patch);
+router.patch(
+	"/updateUserinfo/:userid",
+	auth,
+	upload.single("img"),
+	updateUserInfo.patch
+);
 
 module.exports = router;
