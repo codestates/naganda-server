615302b584a3a8fc85b3f28f60fe01df77e6dfbf
risha <risha939@gmail.com>
2021-03-03 10:48:40 +0900

    [refactor] :userId 엔드포인트 리펙토링

--- controllers/mypage/myScheduleInfo.js
+++ controllers/mypage/myScheduleInfo.js
@@ -1,9 +1,12 @@
 const scheduleModel  = require('../../models/schedule');
+const jwt_decode = require('jwt-decode');
 
 module.exports = {
 	get: async (req, res) => {
 		try {
-            const schedule = await scheduleModel.find({ userInfo: req.params.userid }).select('scheduleTitle thumbnail');
+			const token = req.headers.authorization.split(" ")[1];
+			var decoded = jwt_decode(token);
+            const schedule = await scheduleModel.find({ userInfo: decoded._id }).select('scheduleTitle thumbnail');
             console.log(schedule)
 			return res.status(200).json(schedule);
 		} catch (err) {
--- controllers/mypage/unregister.js
+++ controllers/mypage/unregister.js
@@ -1,21 +1,23 @@
 const usersModel = require("../../models/user");
 const scheduleModel = require("../../models/schedule");
+const jwt_decode = require('jwt-decode');
 
 module.exports = {
 	delete: async (req, res) => {
-		//! 인증은 아직 안함
 		try {
 			/**
 			 * 1. 내가 쓴글 삭제
 			 * 2. 내가 작성한 좋아요/ 싫어요 삭제
 			 * 3. 내정보 삭제
 			 */
-			await scheduleModel.remove({ userInfo: req.params.userid });
+			const token = req.headers.authorization.split(" ")[1];
+			var decoded = jwt_decode(token);
+			await scheduleModel.remove({ userInfo: decoded._id });
 			await scheduleModel.updateMany(
 				{},
-				{ $pull: { bestWorst: { userInfo: req.params.userid } } }
+				{ $pull: { bestWorst: { userInfo: decoded._id } } }
 			);
-			await usersModel.remove({ _id: req.params.userid });
+			await usersModel.remove({ _id: decoded._id });
 			return res
 				.status(200)
 				.json({ message: "정상적으로 회원탈퇴가 처리되었습니다" });
