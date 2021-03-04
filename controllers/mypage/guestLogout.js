b5d7fbded5ea26d7f0d1f846863445abc4f72cb0
박수현 <tngus2057@naver.com>
2021-03-04 16:09:49 +0900

    [fix] 로그아웃 메소드 변경 및 오류나는 파트 주석처리

--- controllers/mypage/guestLogout.js
+++ controllers/mypage/guestLogout.js
@@ -4,54 +4,55 @@ const jwt_decode = require("jwt-decode");
 const { s3 } = require("../../router/multer");
 
 module.exports = {
-	post: async (req, res) => {
+	delete: async (req, res) => {
+		console.log(req.body);
 		try {
 			const token = req.headers.authorization.split(" ")[1];
 			var decoded = jwt_decode(token);
 			//! 스케줄 All 썸네일 버킷 삭제
-			let findSchedule = await scheduleModel.findAll({
-				userInfo: decoded._id,
-			});
-			let allSchedule = findSchedule.map((data) => {
-				return data.thumbnail;
-			});
-			let filterImg = allSchedule.map((arr) => {
-				return arr.map((thumbnail) => {
-					return thumbnail.img;
-				});
-			});
-			let oldImg = filterImg.reduce((a, b) => {
-				return a.concat(b);
-			}, []);
-			let result = oldImg.map((el) => {
-				return { Key: el };
-			});
-			var params = {
-				Bucket: "naganda",
-				Delete: {
-					Objects: result,
-					Quiet: false,
-				},
-			};
-			s3.deleteObjects(params, function (err) {
-				console.log(err);
-			});
-			//! 유저 아바타 버킷 삭제
+			// let findSchedule = await scheduleModel.findAll({
+			// 	userInfo: decoded._id,
+			// });
+			// let allSchedule = findSchedule.map((data) => {
+			// 	return data.thumbnail;
+			// });
+			// let filterImg = allSchedule.map((arr) => {
+			// 	return arr.map((thumbnail) => {
+			// 		return thumbnail.img;
+			// 	});
+			// });
+			// let oldImg = filterImg.reduce((a, b) => {
+			// 	return a.concat(b);
+			// }, []);
+			// let result = oldImg.map((el) => {
+			// 	return { Key: el };
+			// });
+			// var params = {
+			// 	Bucket: "naganda",
+			// 	Delete: {
+			// 		Objects: result,
+			// 		Quiet: false,
+			// 	},
+			// };
+			// s3.deleteObjects(params, function (err) {
+			// 	console.log(err);
+			// });
+			// //! 유저 아바타 버킷 삭제
 			let findInfo = await usersModel.findOne({ _id: decoded._id });
-			await usersModel.findOne({ _id: decoded._id }, (err, data) => {
-				if (data.avatar) {
-					let avatar = data.avatar;
-					s3.deleteObject(
-						{
-							Bucket: "naganda",
-							Key: avatar,
-						},
-						function (err) {
-							console.log(err);
-						}
-					);
-				}
-			});
+			// await usersModel.findOne({ _id: decoded._id }, (err, data) => {
+			// 	if (data.avatar) {
+			// 		let avatar = data.avatar;
+			// 		s3.deleteObject(
+			// 			{
+			// 				Bucket: "naganda",
+			// 				Key: avatar,
+			// 			},
+			// 			function (err) {
+			// 				console.log(err);
+			// 			}
+			// 		);
+			// 	}
+			// });
 			await scheduleModel.remove({ userInfo: decoded._id });
 			await scheduleModel.updateMany(
 				{},
