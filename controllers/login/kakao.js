ef94dfe901c701d1f6b317432941d73b1ad18dce
박수현 <tngus2057@naver.com>
2021-03-06 17:06:29 +0900

    [fix] kakao 소셜로그인 redirect_uri 정보 수정

--- controllers/login/kakao.js
+++ controllers/login/kakao.js
@@ -8,7 +8,7 @@ const qs = require("qs");
 
 module.exports = {
 	post: async (req, res) => {
-		const { code } = req.query;
+		const { code } = req.body.authorizationCode;
 		axios({
 			method: "POST",
 			url: `https://kauth.kakao.com/oauth/token`,
@@ -19,7 +19,7 @@ module.exports = {
 				grant_type: "authorization_code",
 				client_id: clientID,
 				client_secret: clientSecret,
-				redirect_uri: "http://localhost:3000/login",
+				redirect_uri: "http://localhost:3000/signin",
 				code: code,
 			}),
 		})
